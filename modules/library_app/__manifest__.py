e5fbbaddd185aeb6bfed04d62a19d4ff36d10aae
Pavel Savastseika <savaspavel@gmail.com>
2023-07-17 12:48:44 +0400

    -- initialize new module

---
+++ modules/library_app/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Library Management",
+    "description": "Manage library book catalogue and lending.",
+    "author": "Pavel Savastseika/home/pavel/Downloads/icon.png",
+    "depends": ["base"],
+    "application": True,
+}
