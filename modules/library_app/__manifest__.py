622349da8ec9d8e9d5a4d8bd84d61bbbb618bd0a
Pavel Savastseika <savaspavel@gmail.com>
2023-07-17 14:53:41 +0400

    -- add Top-level menu item for the app -- add security groups (users and managers) -- update __manifest__.py

--- modules/library_app/__manifest__.py
+++ modules/library_app/__manifest__.py
@@ -4,4 +4,8 @@
     "author": "Pavel Savastseika/home/pavel/Downloads/icon.png",
     "depends": ["base"],
     "application": True,
+    "data": [
+        "security/library_security.xml",
+        "views/library_menu.xml"
+    ]
 }
